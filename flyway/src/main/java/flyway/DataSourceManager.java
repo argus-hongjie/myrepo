2de25e8721ff82fdd2ea89b23a507b590139e4bb
argus-hongjie <hongjiezhang@hongjie.local>
2017-01-23 20:47:19 +0100

    flyway

---
+++ flyway/src/main/java/flyway/DataSourceManager.java
@@ -0,0 +1,70 @@
+package flyway;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import org.postgresql.ds.PGPoolingDataSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+
+public class DataSourceManager {
+
+	String host="localhost";
+	Integer port=5432;
+	String database="flyway";
+	String user="dbuser";
+	String password="dbuser";
+	Integer poolSize=10;
+			
+	
+	private static PGPoolingDataSource source = new PGPoolingDataSource();
+	private final NamedParameterJdbcTemplate jdbcTemplate;
+
+	private DataSourceManager() {
+		init();
+		jdbcTemplate = new NamedParameterJdbcTemplate(source);
+	}
+
+	private static class DataSourceManagerHolder {
+		private static final DataSourceManager instance = new DataSourceManager();
+	}
+
+	/**
+	 * Initialisation de pool de connexion.
+	 */
+	private void init() {
+		try {
+			source.setDataSourceName("name");
+			source.setServerName(host);
+			source.setDatabaseName(database);
+			source.setUser(user);
+			source.setPassword(password);
+			source.setMaxConnections(poolSize);
+		} catch (NumberFormatException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public NamedParameterJdbcTemplate getJdbcTemplate() {
+		return jdbcTemplate;
+	}
+	
+	/**
+	 * récupérer une connecxion depuis le pool de connexion.
+	 * 
+	 * @return Une connexion avec la base de données.
+	 */
+	public Connection getConnection() {
+		try {
+			return source.getConnection();
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
+
+	}
+
+	public static DataSourceManager getInstance() {
+		return DataSourceManagerHolder.instance;
+	}
+}
---
+++ flyway/src/test/java/flyway/MainTest.java
@@ -0,0 +1,31 @@
+package flyway;
+
+import java.util.HashMap;
+
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+public class MainTest extends TestCase{
+
+	@Test
+	public void testGetConnection() {
+		assertNotNull(DataSourceManager.getInstance().getConnection());
+	}
+
+	/**
+	 * Test method for {@link fr.argus.socle.db.DataSourceManager#getInstance()}
+	 * .
+	 */
+	@Test
+	public void testGetInstance() {
+		assertNotNull(DataSourceManager.getInstance());
+	}
+	
+	@Test void testInsertUser() {
+		DataSourceManager.getInstance().getJdbcTemplate().update("Insert into users(email) values('a@a.fr')", new HashMap());
+		String email = DataSourceManager.getInstance().getJdbcTemplate().queryForObject("select email from users where email='a@a.fr'", new HashMap(), String.class);
+		assertTrue(email.equals("a@a.fr"));
+	}
+
+}

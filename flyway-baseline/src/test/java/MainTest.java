86fba5c4e0907839a6b04e2131fa93afe073af55
argus-hongjie <hongjie.zhang@rse.argus.int>
2017-01-25 18:19:27 +0100

    a

--- flyway-baseline/src/test/java/MainTest.java
+++ flyway-baseline/src/test/java/MainTest.java
@@ -39,11 +39,35 @@ public class MainTest extends TestCase{
 		flyway.setBaselineVersionAsString("1.0.1");
 		flyway.migrate();
 		
-		Assertions.assertThatThrownBy(()->DataSourceManager.getInstance().getJdbcTemplate().queryForObject("select count(*) from users", new HashMap(), Integer.class)).isInstanceOf(BadSqlGrammarException.class);
-		assertTrue(DataSourceManager.getInstance().getJdbcTemplate().queryForObject("select count(*) from users2", new HashMap(), Integer.class)==1);
-		assertTrue(DataSourceManager.getInstance().getJdbcTemplate().queryForObject("select count(*) from a", new HashMap(), Integer.class)==0);
-		assertTrue(DataSourceManager.getInstance().getJdbcTemplate().queryForObject("select count(*) from b", new HashMap(), Integer.class)==0);
+		List<Map<String, Object>> list = DataSourceManager.getInstance().getJdbcTemplate().queryForList("select * from SCHEMA_VERSION", new HashMap());
+		list.stream().forEach(map->{
+				System.out.println("-------------------------------------------");
+				System.out.println("installed_rank: " + map.get("installed_rank"));
+				System.out.println("version: " + map.get("version"));
+				System.out.println("description: " + map.get("description"));
+				System.out.println("type: " + map.get("type"));
+				System.out.println("script: " + map.get("script"));
+				System.out.println("checksum: " + map.get("checksum"));
+				System.out.println("installed_by: " + map.get("installed_by"));
+				System.out.println("installed_on: " + map.get("installed_on"));
+				System.out.println("execution_time: " + map.get("execution_time"));
+				System.out.println("success: " + map.get("success"));
+			});
 		Assertions.assertThat(DataSourceManager.getInstance().getJdbcTemplate().queryForObject("select count(*) from SCHEMA_VERSION", new HashMap(), Integer.class)).isEqualTo(3);
+		assertTrue(DataSourceManager.getInstance().getJdbcTemplate().queryForObject("select count(*) from users2", new HashMap(), Integer.class)==1);
+		Assertions.assertThatThrownBy(()->DataSourceManager.getInstance().getJdbcTemplate().queryForObject("select count(*) from users", new HashMap(), Integer.class)).isInstanceOf(BadSqlGrammarException.class);
+		Assertions.assertThatThrownBy(()->DataSourceManager.getInstance().getJdbcTemplate().queryForObject("select count(*) from a", new HashMap(), Integer.class)).isInstanceOf(BadSqlGrammarException.class);
+		assertTrue(DataSourceManager.getInstance().getJdbcTemplate().queryForObject("select count(*) from b", new HashMap(), Integer.class)==0);
+	}
+	
+	@Test 
+	public void testInsertUser2() {
+		Flyway flyway = new Flyway();
+		flyway.setDataSource(DataSourceManager.getInstance().getSource());
+		flyway.setBaselineOnMigrate(true);
+		flyway.setBaselineVersionAsString("1.0.1");
+		flyway.migrate();
+		
 		List<Map<String, Object>> list = DataSourceManager.getInstance().getJdbcTemplate().queryForList("select * from SCHEMA_VERSION", new HashMap());
 		list.stream().forEach(map->{
 				System.out.println("-------------------------------------------");
@@ -59,6 +83,10 @@ public class MainTest extends TestCase{
 				System.out.println("success: " + map.get("success"));
 			});
 	
+		assertTrue(DataSourceManager.getInstance().getJdbcTemplate().queryForObject("select count(*) from users", new HashMap(), Integer.class)==0);
+		assertTrue(DataSourceManager.getInstance().getJdbcTemplate().queryForObject("select count(*) from a", new HashMap(), Integer.class)==0);
+		assertTrue(DataSourceManager.getInstance().getJdbcTemplate().queryForObject("select count(*) from b", new HashMap(), Integer.class)==0);
+		Assertions.assertThat(DataSourceManager.getInstance().getJdbcTemplate().queryForObject("select count(*) from SCHEMA_VERSION", new HashMap(), Integer.class)).isEqualTo(3);
 	}
 
 }

761b955e6d8cfdf14f7ce14aa3ddb69c3a082d9e
argus-hongjie <hongjie.zhang@rse.argus.int>
2017-01-26 11:24:13 +0100

    asdf

--- flyway-baseline/src/test/java/MainTest.java
+++ flyway-baseline/src/test/java/MainTest.java
@@ -34,7 +34,7 @@ public class MainTest extends TestCase{
 		flyway.clean();
 		DataSourceManager.getInstance().getJdbcTemplate().update("CREATE TABLE users2 (id SERIAL PRIMARY KEY, email TEXT)", new HashMap());
 		DataSourceManager.getInstance().getJdbcTemplate().update("Insert into users2(email) values('a@a.fr')", new HashMap());
-		flyway.setBaselineOnMigrate(true); // without_baselineVersion_default_V1: not execute first sql
+		flyway.setBaselineOnMigrate(true); // without_baselineVersion_default_V1: not execute V1__Xx.sql, V1.0__Xx.sql, V1.0.0__Xx.sql, but execute V1.1__Xx.sql, V1.0.1__Xx.sql
 		flyway.migrate();
 		
 		List<Map<String, Object>> list = DataSourceManager.getInstance().getJdbcTemplate().queryForList("select * from SCHEMA_VERSION", new HashMap());
@@ -52,7 +52,7 @@ public class MainTest extends TestCase{
 				System.out.println("execution_time: " + map.get("execution_time"));
 				System.out.println("success: " + map.get("success"));
 			});
-		Assertions.assertThat(DataSourceManager.getInstance().getJdbcTemplate().queryForObject("select count(*) from SCHEMA_VERSION", new HashMap(), Integer.class)).isEqualTo(2);
+		Assertions.assertThat(DataSourceManager.getInstance().getJdbcTemplate().queryForObject("select count(*) from SCHEMA_VERSION", new HashMap(), Integer.class)).isEqualTo(4);
 		assertTrue(DataSourceManager.getInstance().getJdbcTemplate().queryForObject("select count(*) from users2", new HashMap(), Integer.class)==1);
 		Assertions.assertThatThrownBy(()->DataSourceManager.getInstance().getJdbcTemplate().queryForObject("select count(*) from users", new HashMap(), Integer.class)).isInstanceOf(BadSqlGrammarException.class);
 		assertTrue(DataSourceManager.getInstance().getJdbcTemplate().queryForObject("select count(*) from a", new HashMap(), Integer.class)==0);

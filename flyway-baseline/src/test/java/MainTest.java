1129689dd844519d27eaa03dd5becbeb0cc7a680
argus-hongjie <hongjie.zhang@rse.argus.int>
2017-01-26 11:30:28 +0100

    sadf

--- flyway-baseline/src/test/java/MainTest.java
+++ flyway-baseline/src/test/java/MainTest.java
@@ -54,7 +54,8 @@ public class MainTest extends TestCase{
 			});
 		Assertions.assertThat(DataSourceManager.getInstance().getJdbcTemplate().queryForObject("select count(*) from SCHEMA_VERSION", new HashMap(), Integer.class)).isEqualTo(4);
 		assertTrue(DataSourceManager.getInstance().getJdbcTemplate().queryForObject("select count(*) from users2", new HashMap(), Integer.class)==1);
-		Assertions.assertThatThrownBy(()->DataSourceManager.getInstance().getJdbcTemplate().queryForObject("select count(*) from users", new HashMap(), Integer.class)).isInstanceOf(BadSqlGrammarException.class);
+//		Assertions.assertThatThrownBy(()->DataSourceManager.getInstance().getJdbcTemplate().queryForObject("select count(*) from users", new HashMap(), Integer.class)).isInstanceOf(BadSqlGrammarException.class);
+		assertTrue(DataSourceManager.getInstance().getJdbcTemplate().queryForObject("select count(*) from users", new HashMap(), Integer.class)==0); // first sql: V1.0.1__Create_tables.sql
 		assertTrue(DataSourceManager.getInstance().getJdbcTemplate().queryForObject("select count(*) from a", new HashMap(), Integer.class)==0);
 		assertTrue(DataSourceManager.getInstance().getJdbcTemplate().queryForObject("select count(*) from b", new HashMap(), Integer.class)==0);
 	}
@@ -64,7 +65,7 @@ public class MainTest extends TestCase{
 		Flyway flyway = new Flyway();
 		flyway.setDataSource(DataSourceManager.getInstance().getSource());
 		flyway.clean();
-		flyway.setBaselineOnMigrate(true);
+		flyway.setBaselineOnMigrate(true); // without_baselineVersion_default_V1: not execute V1__Xx.sql, V1.0__Xx.sql, V1.0.0__Xx.sql, but execute V1.1__Xx.sql, V1.0.1__Xx.sql
 		flyway.migrate();
 		
 		List<Map<String, Object>> list = DataSourceManager.getInstance().getJdbcTemplate().queryForList("select * from SCHEMA_VERSION", new HashMap());
@@ -96,7 +97,7 @@ public class MainTest extends TestCase{
 		DataSourceManager.getInstance().getJdbcTemplate().update("CREATE TABLE users2 (id SERIAL PRIMARY KEY, email TEXT)", new HashMap());
 		DataSourceManager.getInstance().getJdbcTemplate().update("Insert into users2(email) values('a@a.fr')", new HashMap());
 		flyway.setBaselineOnMigrate(true);
-		flyway.setBaselineVersionAsString("2.0.1"); // not execute V1.0.1__XXX.sql
+		flyway.setBaselineVersionAsString("2.0.1"); // not execute V2.0.1__XXX.sql
 		flyway.migrate();
 		
 		List<Map<String, Object>> list = DataSourceManager.getInstance().getJdbcTemplate().queryForList("select * from SCHEMA_VERSION", new HashMap());
@@ -128,7 +129,7 @@ public class MainTest extends TestCase{
 		flyway.setDataSource(DataSourceManager.getInstance().getSource());
 		flyway.clean();
 		flyway.setBaselineOnMigrate(true);
-		flyway.setBaselineVersionAsString("2.0.1");
+		flyway.setBaselineVersionAsString("2.0.1");// not execute V2.0.1__XXX.sql
 		flyway.migrate();
 		
 		List<Map<String, Object>> list = DataSourceManager.getInstance().getJdbcTemplate().queryForList("select * from SCHEMA_VERSION", new HashMap());
